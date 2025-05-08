669de3ce4c0b52e83667bb30ff3c671a41bf929b
yafidaffaa <yafi.d.andriansyah@gmail.com>
2025-05-09 01:25:38 +0700

    menambhakan bloc builder dan eleveted button

--- lib/presentation/todo_page.dart
+++ lib/presentation/todo_page.dart
@@ -9,6 +9,56 @@ class TodoPage extends StatelessWidget {
   Widget build(BuildContext context) {
     final _key = GlobalKey<FormState>();
     final _controller = TextEditingController();
-    return Scaffold();
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              Text('Todo List'),
+              Row(
+                children: [
+                  Column(
+                    children: [
+                      Text('Selected Date'),
+                      BlocBuilder<TodoBlocBloc, TodoBlocState>(
+                        builder: (context, state) {
+                          if (state is TodoLoaded) {
+                            if (state.selectedDate != null) {
+                              return Text(
+                                '${state.selectedDate!.day}/${state.selectedDate!.month}/${state.selectedDate!.year}',
+                              );
+                            }
+                          }
+                          return Text('No date selected');
+                        },
+                      ),
+                      SizedBox(height: 16.0),
+                      ElevatedButton(
+                        onPressed: () {
+                          showDatePicker(
+                            context: context,
+                            initialDate: DateTime.now(),
+                            firstDate: DateTime(2000),
+                            lastDate: DateTime(2100),
+                          ).then((selectedDate) {
+                            if (selectedDate != null) {
+                              context.read<TodoBlocBloc>().add(
+                                TodoSelectedDate(date: selectedDate),
+                              );
+                            }
+                          });
+                        },
+                        child: Text('Select Date'),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
