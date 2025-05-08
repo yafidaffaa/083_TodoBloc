e645f996c2a4ef4d3968f5c835722b6c0a2eac28
yafidaffaa <yafi.d.andriansyah@gmail.com>
2025-05-09 01:14:05 +0700

    manambahkan bloc add

---
+++ lib/bloc/todo_bloc_bloc.dart
@@ -0,0 +1,25 @@
+import 'package:bloc/bloc.dart';
+import 'package:state_todo_bloc/model/todo.dart';
+
+part 'todo_bloc_event.dart';
+part 'todo_bloc_state.dart';
+
+class TodoBlocBloc extends Bloc<TodoBlocEvent, TodoBlocState> {
+  TodoBlocBloc() : super(TodoLoaded(todos: [], selectedDate: null)) {
+    on<TodoBlocEventAdd>((event, emit) {
+      final currentState = state;
+      if (currentState is TodoLoaded) {
+        final List<Todo> updatedTodos = List.from(currentState.todos);
+        updatedTodos.add(
+          Todo(title: event.title, date: event.date, isCompleted: false),
+        );
+        emit(
+          TodoLoaded(
+            todos: updatedTodos,
+            selectedDate: currentState.selectedDate,
+          ),
+        );
+      }
+    });
+  }
+}
